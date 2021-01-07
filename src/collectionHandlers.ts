1bb16f9f7fff94d1ff0008036daae197d0a8b052
Ronan <liruonan@hotmail.com>
2021-01-07 23:37:59 +0800

    feat(collectionHandlers)

--- src/collectionHandlers.ts
+++ src/collectionHandlers.ts
@@ -1,3 +1,25 @@
+/**
+ * 核心还是完成 ProxyHandler的特性
+ * get
+ * set
+ * has
+ * ownKey
+ * deleteProperty
+ */
+
+import { toRaw, reactive, readonly, isObject, hasOwn } from './reactive';
+import { track, trigger } from './effect';
+import { OperationTypes } from './OperationTypes';
+import { LOCKED } from './lock'
+
+function toReactive(value: any) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+function toReadonly(value: any) {
+  return isObject(value) ? readonly(value) : value;
+}
+
 
 export const mutableCollectionHandlers: ProxyHandler<any> = {}
 
