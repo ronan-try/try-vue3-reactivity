3c1f72594a1f980a1fdc84b836fa59d373ae1c8c
Ronan <liruonan@hotmail.com>
2021-01-13 08:50:21 +0800

    feat(collectionHandler): get has size

--- src/collectionHandlers.ts
+++ src/collectionHandlers.ts
@@ -20,6 +20,57 @@ function toReadonly(value: any) {
   return isObject(value) ? readonly(value) : value;
 }
 
+function get(target: any, key: any, wrap:(t:any) => any):any {
+  // 1. 拿到原始材料original
+  target = toRaw(target);
+  key = toRaw(key);
+
+  // 2. 得到collection的原型
+  const proto: any = Reflect.getPrototypeOf(target);
+
+  // 跟踪一下，为effect准备
+  // 为什么get时，跟踪？？？
+  /**
+   * 难道尽在track时跟踪吗？trigger是不是也得跟踪呢？比如：
+   * observed[0] = 100; 赋值行为：先触发get，还是set？看实验结果：
+   * 直接触发set操作，不会触发get
+   * 所以一会儿看一下set函数，有没有跟踪/或者是在trigger跟踪了？？？
+   * */
+  track(target, OperationTypes.GET, key);
+
+  // 3. 调用原型的get方法
+  const result = proto.get.call(target, key);
+  return wrap(result);
+}
+
+function has(this: any, key: any): boolean {
+  // 拿到原始材料
+  const target = toRaw(this);
+  key = toRaw(key);
+
+  // 拿到原型
+  const proto: any = Reflect.getPrototypeOf(target);
+
+  // 擦，很智慧呢
+  // track都是original的，这样能减少target有多个代理 或者 被称为属性
+  track(target, OperationTypes.HAS, key);
+
+  // 调用原型方法
+  return proto.has.call(target, key);
+}
+
+
+function size(target: any) {
+  target = toRaw(target);
+  const proto = Reflect.getPrototypeOf(target);
+
+  track(target, OperationTypes.ITERATE);
+
+  // 又是一个智慧的使用
+  return Reflect.get(proto, 'size', target);
+
+  // 我还以为要proto.size.call()呢，仔细想想size是属性
+}
 
 export const mutableCollectionHandlers: ProxyHandler<any> = {}
 
--- src/try.reactive.ts
+++ src/try.reactive.ts
@@ -13,70 +13,72 @@ const observed = new Proxy(arr, {
   }
 })
 
-// console.log(arr[0]);
-// console.log(observed[0]);
-// console.log('-------------------');
+observed[0] = 1000;
 
-let baz = { baz: 1};
-observed.push(...[baz, baz]);
+// // console.log(arr[0]);
+// // console.log(observed[0]);
+// // console.log('-------------------');
 
-console.table(arr);
+// let baz = { baz: 1};
+// observed.push(...[baz, baz]);
+
+// console.table(arr);
+
+// // {
+// //   let arr = [1, 2, 3]
+// //   let proxy = new Proxy(arr, {
+// //     get (target, prop, receiver) {
+// //       console.log('get', prop);
+// //       return target[prop];
+// //     },
+// //     set (target, prop, val, receiver) {
+// //       console.log('set', prop, val);
+// //       return true;
+// //     }
+// //   });
+
+// //   proxy.push(2323);
+// //   console.log('---')
+// //   typeof proxy.push
+// // }
 
 // {
-//   let arr = [1, 2, 3]
-//   let proxy = new Proxy(arr, {
+//   console.log('我是分割线');
+//   const nested = {
+//     foo: {
+//       bar: { zz: 0 },
+//       baz: 2
+//     }
+//   };
+//   const proxy = new Proxy(nested, {
 //     get (target, prop, receiver) {
-//       console.log('get', prop);
-//       return target[prop];
+//       console.log('get target: ', target);
+//       console.log('get receiver: ', receiver === proxy);
+//       console.log('get: ', prop);
+//       console.log('get val:', target[prop]);
+//       return Reflect.get(target, prop, receiver);
 //     },
-//     set (target, prop, val, receiver) {
-//       console.log('set', prop, val);
-//       return true;
+//     set (target, prop, val) {
+//       console.log('set: ', prop, val);
+//       return Reflect.set(target, prop, val);
 //     }
 //   });
 
-//   proxy.push(2323);
-//   console.log('---')
-//   typeof proxy.push
+//   console.log(proxy.foo.bar.zz);
+//   // console.log(proxy.foo.baz);
+//   console.log('我是分割线-------------');
+//   // proxy.foo.bar = 100;
+//   // console.log('------------', nested.foo.bar);
+//   // console.log(proxy.foo.bar);
+
+//   // console.log('nested == proxy', nested == proxy);
+//   // console.log('nested.foo == proxy.foo', nested.foo == proxy.foo);
+//   // console.log('nested.foo.bar == proxy.foo.bar', nested.foo.bar == proxy.foo.bar);
+
+//   console.log('分割线**********************************');
+//   Reflect.set(proxy, 'aaaaaaa', '10000000');
+//   console.log('分割线---------------------------------');
+//   console.log(nested['aaaaaaa']);
+//   console.log(proxy['aaaaaaa']);
+//   console.log(proxy);
 // }
-
-{
-  console.log('我是分割线');
-  const nested = {
-    foo: {
-      bar: { zz: 0 },
-      baz: 2
-    }
-  };
-  const proxy = new Proxy(nested, {
-    get (target, prop, receiver) {
-      console.log('get target: ', target);
-      console.log('get receiver: ', receiver === proxy);
-      console.log('get: ', prop);
-      console.log('get val:', target[prop]);
-      return Reflect.get(target, prop, receiver);
-    },
-    set (target, prop, val) {
-      console.log('set: ', prop, val);
-      return Reflect.set(target, prop, val);
-    }
-  });
-
-  console.log(proxy.foo.bar.zz);
-  // console.log(proxy.foo.baz);
-  console.log('我是分割线-------------');
-  // proxy.foo.bar = 100;
-  // console.log('------------', nested.foo.bar);
-  // console.log(proxy.foo.bar);
-
-  // console.log('nested == proxy', nested == proxy);
-  // console.log('nested.foo == proxy.foo', nested.foo == proxy.foo);
-  // console.log('nested.foo.bar == proxy.foo.bar', nested.foo.bar == proxy.foo.bar);
-
-  console.log('分割线**********************************');
-  Reflect.set(proxy, 'aaaaaaa', '10000000');
-  console.log('分割线---------------------------------');
-  console.log(nested['aaaaaaa']);
-  console.log(proxy['aaaaaaa']);
-  console.log(proxy);
-}
